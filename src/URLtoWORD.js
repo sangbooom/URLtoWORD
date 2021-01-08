a962e84d31bd446625b0bca1fc4bc2b53331f702
sangbooom <tkdqja4164@naver.com>
2021-01-08 17:32:51 +0900

    feat: urlToWord 기능 완성

--- src/URLtoWORD.js
+++ src/URLtoWORD.js
@@ -4,26 +4,106 @@ import "./URLtoWORD.css";
 import firebase from "./firebase";
 
 const { Search, TextArea } = Input;
-// const wordArray = ["사과","수박","딸기","참외","귤","포도","자몽","감귤","멜론","체리","레몬","망고","키위","석류","자두","앵두","매실","모과","살구","대추","쥐","소","말","양","강아지","고양이"];
-// let nummberArray = [];
-// for(let i=0; i<1000; i++){
-//     nummberArray.push(i);
-// }
+const wordArray = ["사과","수박","딸기","참외","귤","포도","자몽","바나나","멜론","체리","레몬","망고","키위","석류"];
+let nummberArray = [];
+for(let i=0; i<=2; i++){
+    nummberArray.push(i);
+}
 
 const URLtoWORD = () => {
-  const [changedWord, setChangedWord] = useState("");
-  const [datas] = useState([]);
-  const [count, setCount] = useState(0);
+  const [datas, setDatas] = useState("");
+  const [isLoading, setLoading] = useState(false);
   const [urlToWordValue, setUrlToWordValue] = useState("");
   const [wordToUrlValue, setWordToUrlValue] = useState("");
+  const [numberIndex, setNumberIndex] = useState(0);
+  const [wordIndex, setWordIndex] = useState(0);
+
+  // 초기값 데이터베이스에 생성
+  // useEffect(()=>{
+  //   firebase.database().ref("currentIndex").set({
+  //     word: 0,
+  //     number: 0,
+  //   });
+  // },[]);
+
+  // useEffect(() => {
+  //   setLoading(true);
+  //   //데이터베이스에서 값 읽기
+  //   firebase
+  //     .database()
+  //     .ref("currentIndex")
+  //     .once("value", (snapshot) => {
+  //       console.log(snapshot.val());
+  //       // if(snapshot.val().number === 100){
+  //       //   setDatas(
+  //       //     wordArray[snapshot.val().word+1] + nummberArray[0]
+  //       //   );
+  //       //   setWordIndex(snapshot.val().word+1);
+  //       //   setNumberIndex(0);
+  //       //   return;
+  //       // }
+
+  //       setDatas(
+  //         wordArray[snapshot.val().word] + nummberArray[snapshot.val().number]
+  //       );
+  //       setWordIndex(snapshot.val().word);
+  //       setNumberIndex(snapshot.val().number);
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    console.log(datas);
+    setLoading(false);
+  }, [datas]);
 
   const onChangeUrlToWord = (value) => {
-    //firebase 데이터베이스에 저장
-    firebase.database().ref("datas").child("수박13").set({
+    // 데이터베이스에 저장
+    firebase.database().ref("datas").child(datas).set({
       url: value,
-      word: "수박13",
-    }); //////////////////////////// 수박13을 랜덤값으로 바꾸기
+      word: datas,
+    });
+    // currentIndex.number++ , 만약 100이면 %100하고 currentIndex.word++
+    setWordToUrlValue(datas);
+    setNumberIndex(numberIndex + 1);
+    
   };
+
+  useEffect(()=>{
+    firebase.database().ref("currentIndex").set({
+      word: wordIndex,
+      number: numberIndex,
+    },(error) => {
+      if (error) {
+        console.log(error);
+      } else {
+        // Data saved successfully!
+        firebase
+      .database()
+      .ref("currentIndex")
+      .once("value", (snapshot) => {
+        console.log(snapshot.val());
+        if(snapshot.val().number === 2){
+          setDatas(
+            wordArray[snapshot.val().word+1] + nummberArray[0]
+          );
+          setWordIndex(snapshot.val().word+1);
+          setNumberIndex(0);
+          return;
+        }
+
+        setDatas(
+          wordArray[snapshot.val().word] + nummberArray[snapshot.val().number]
+        );
+        setWordIndex(snapshot.val().word);
+        setNumberIndex(snapshot.val().number);
+      });
+      }
+    });
+  },[numberIndex,wordIndex])
+
+  useEffect(()=>{
+    console.log(numberIndex);
+  },[numberIndex])
 
   const onChangeWordToUrl = (value) => {
     //firebase 데이터베이스에서 가져옴
@@ -33,15 +113,16 @@ const URLtoWORD = () => {
       .ref("datas")
       .once("value", (snapshot) => {
         if (!snapshot.val()[value]) {
-          console.log("결과 없음");
           setUrlToWordValue("결과 없음");
           return;
         }
         setUrlToWordValue(snapshot.val()[value].url);
-        // setCount(Object.keys(snapshot.val()).length);
-        // console.log(count)
       });
   };
+
+  if (isLoading) {
+    return <div>{"로딩중입니다"}</div>;
+  }
 
   return (
     <div className="wrapper">
@@ -56,7 +137,7 @@ const URLtoWORD = () => {
         onSearch={onChangeUrlToWord}
         style={{ marginBottom: 20 }}
       />
-      <TextArea rows={4} readOnly value={"asd"} />
+      <TextArea rows={4} readOnly value={wordToUrlValue} />
 
       <div style={{ marginBottom: 40 }} />
 
